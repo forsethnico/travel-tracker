c91199fc95b35aad168133df2974d162723fe489
Nicole Forseth <forseth.nico@gmail.com>
2022-09-21 15:33:28 -0600

    Add GET fetch calls for travel data

---
+++ src/apiCalls.js
@@ -0,0 +1,30 @@
+const fetchTraveler = (id) => {
+  return fetch(`http://localhost:3001/api/v1/travelers/${id}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error("User does not exist.");
+      } else {
+        return response.json();
+      }
+    }
+  );
+};
+
+const fetchTravelData = (detail) => {
+  return fetch(`http://localhost:3001/api/v1/${detail}`)
+    .then((response) => response.json())
+    .catch((error) => console.log(`API error: ${error.message}`));
+};
+
+const getTravelData = () => {
+  const result = Promise.all([
+    fetchTravelData("travelers"),
+    fetchTravelData("trips"),
+    fetchTravelData("destinations"),
+  ]).then((responses) => {
+    return responses;
+  });
+  return result;
+};
+
+export { getTravelData, fetchTraveler };
